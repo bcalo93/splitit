b8f7b377160dd65e5549128f0b2f1ae77071316b
Brahian Calo <bcalo@192.168.1.6>
2025-09-22 11:16:31 -0300

    init project

---
+++ composeApp/src/commonMain/kotlin/com/example/splitit/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.splitit
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/example/splitit/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.splitit
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
