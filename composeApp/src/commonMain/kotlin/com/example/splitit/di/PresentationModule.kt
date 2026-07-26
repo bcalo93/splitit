5e746bcc3f146f1147da4033b39b123d8f2debb4
Brahian  Calo <bcalo93@gmail.com>
2026-07-26 12:16:32 -0300

    feat: phase 4 completed (#7)

--- composeApp/src/commonMain/kotlin/com/example/splitit/di/PresentationModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/splitit/di/PresentationModule.kt
@@ -1,6 +1,7 @@
 package com.example.splitit.di
 
 import com.example.splitit.domain.value.SessionId
+import com.example.splitit.presentation.participants.ParticipantsViewModel
 import com.example.splitit.presentation.sessiondetail.SessionDetailsViewModel
 import com.example.splitit.presentation.sessions.SessionFormViewModel
 import com.example.splitit.presentation.sessions.SessionListViewModel
@@ -11,4 +12,5 @@ val presentationModule = module {
     viewModel { SessionListViewModel(get(), get()) }
     viewModel { (sessionId: SessionId?) -> SessionFormViewModel(sessionId, get(), get(), get()) }
     viewModel { (sessionId: SessionId) -> SessionDetailsViewModel(sessionId, get()) }
+    viewModel { (sessionId: SessionId) -> ParticipantsViewModel(sessionId, get(), get(), get(), get()) }
 }
